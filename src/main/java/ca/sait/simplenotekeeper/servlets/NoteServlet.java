2f6932d676139a284a04e425c6d694fb5a196111
Yoonju Baek <mbibe91@gmail.com>
2022-01-30 15:38:44 -0700

    final development

--- src/main/java/ca/sait/simplenotekeeper/servlets/NoteServlet.java
+++ src/main/java/ca/sait/simplenotekeeper/servlets/NoteServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.*;
  */
 public class NoteServlet extends HttpServlet {
 
+    private static final String noteFilePath = "/WEB-INF/note.txt";
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -22,22 +23,22 @@ public class NoteServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String path = getServletContext().getRealPath(noteFilePath);
+        // to read files
+        BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+
+        String title = br.readLine();
+        String contents = br.readLine();
+
+        Note note = new Note(title, contents);
+        request.setAttribute("note", note);
+            
         String query = request.getQueryString();
         
-        if(query != null && query.contains("edit")) {
+        if(query != null && query.contains("edit")) {    
             getServletContext().getRequestDispatcher("/WEB-INF/editnote.jsp").forward(request, response);
         }
         else {
-            String path = getServletContext().getRealPath("/WEB-INF/note.txt");
-            // to read files
-            BufferedReader br = new BufferedReader(new FileReader(new File(path)));
-
-            String title = br.readLine();
-            String contents = br.readLine();
-
-            Note note = new Note(title, contents);
-            request.setAttribute("note", note);
-
             getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response);
         }
     }
@@ -56,7 +57,7 @@ public class NoteServlet extends HttpServlet {
         String title = request.getParameter("title");
         String contents = request.getParameter("contents");
         
-        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
+        String path = getServletContext().getRealPath(noteFilePath);
         
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path, false))); 
         
--- src/main/webapp/WEB-INF/editnote.jsp
+++ src/main/webapp/WEB-INF/editnote.jsp
@@ -1,9 +1,9 @@
 <%-- 
     Document   : editnote
     Created on : Jan 30, 2022, 11:08:13 AM
-    Author     : mbibe
+    Author     : Yoonju Baek
 --%>
-
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -17,11 +17,11 @@
         <form action="note" method="post">
             <div>
                 <label for="title">Title:</label>
-                <input type="text" id="title" name="title" placeholder="Title">
+                <input type="text" id="title" name="title" placeholder="Title" value="${note.title}">
             </div>
             <div>
                 <label id="contents">Contents:</label>
-                <textarea id="contents" name="contents" placeholder="Contents"></textarea>
+                <textarea rows="6" id="contents" name="contents" placeholder="Contents"><c:out value="${note.contents}"/></textarea>
             </div>
             <button type="submit">Save</button>
         </form>
